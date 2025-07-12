9d1fe1110308b00793dfeadbb29a17886ead5267
Anjo Tadena <tadena.anjo@gmail.com>
2025-07-12 16:11:55 +0800

    Setup integration testing

---
+++ tests/IntegrationTests/TestSetup.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+using Infrastructure.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+
+namespace IntegrationTests;
+
+public static class TestSetup
+{
+    public static IServiceProvider BuildServiceProvider()
+    {
+        var services = new ServiceCollection();
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseInMemoryDatabase("TestDb_NovaStarterKit");
+        });
+
+        services.AddScoped<IRepository<User, Guid>, GenericRepository<User, Guid>>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserService, UserService>();
+
+        return services.BuildServiceProvider();
+    }
+}
---
+++ tests/IntegrationTests/Users/UserServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntegrationTests.Users;
+
+public class UserServiceTests
+{
+    private readonly IUserService _userService;
+
+    public UserServiceTests()
+    {
+        var provider = TestSetup.BuildServiceProvider();
+
+        _userService = provider.GetRequiredService<IUserService>();
+    }
+
+    [Fact]
+    public async Task CreateUser_ShouldReturnUserDto_WithValidInput()
+    {
+        // Arrange
+        var firstName = "Test";
+        var lastName = "User";
+        var email = "test.user@email.co";
+        var password = "password";
+        var role = "User";
+
+        // Act
+        var result = await _userService.CreateUserAsync(firstName, lastName, email, password, role);
+
+        Assert.NotNull(result);
+        Assert.Equal(firstName, result.FirstName);
+        Assert.Equal(lastName, result.LastName);
+        Assert.Equal(email, result.Email);
+        Assert.Equal(role, result.Role);
+    }
+}
