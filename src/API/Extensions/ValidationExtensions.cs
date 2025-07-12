905b4469189b2b7e9ee56befaa5953692594edb3
Anjo Tadena <tadena.anjo@gmail.com>
2025-07-12 15:47:55 +0800

    Add FluentValidation and implemente createUser validation

---
+++ src/API/Extensions/ValidationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using API.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
+namespace API.Extensions;
+
+public static class ValidationExtensions
+{
+    public static IServiceCollection AddValidations(this IServiceCollection services)
+    {
+        services.AddFluentValidationAutoValidation();
+
+        services.AddValidatorsFromAssemblyContaining<CreateUserRequestValidator>();
+
+        return services;
+    }
+}
---
+++ src/API/Validators/CreateUserRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using API.Models.Requests;
+using FluentValidation;
+
+namespace API.Validators;
+
+public class CreateUserRequestValidator : AbstractValidator<CreateUserRequest>
+{
+    public CreateUserRequestValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .MaximumLength(100);
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("First name is required")
+            .MaximumLength(100);
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("First name is required")
+            .EmailAddress().WithMessage("Email format is valid");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters");
+
+        RuleFor(x => x.Role)
+            .Must(role => new[] { "User", "Admin" }.Contains(role))
+            .WithMessage("Role must be either 'User' or 'Admin'");
+    }
+}
